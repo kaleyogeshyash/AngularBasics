025d1d16ce3022116f815eb5ddc88c3fa9c117f8
kaleyogeshyash <yogeshdilip.k@hcl.com>
2023-01-18 17:49:00 +0530

    First Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { Assignment2Component } from './assignment2/assignment2.component';
+import { FormsModule } from '@angular/forms';
+import { Assignment3Component } from './assignment3/assignment3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent,
+    Assignment2Component,
+    Assignment3Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
